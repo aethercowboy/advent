05bb6e14f9fe6377a7547c6cf049d2fbea76f49e
Jacob P. Silvia <jacob.silvia@gmail.com>
2017-12-01 10:18:43 -0500

    Day 01

---
+++ advent2017.tests/Days/Day01Tests.cs
@@ -0,0 +1,81 @@
+using advent2017.Days;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace advent2017.tests.Days
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Day01 _client;
+
+        [TestInitialize]
+        public void Startup()
+        {
+            _client = new Day01();
+        }
+
+        [TestMethod]
+        public void Test01()
+        {
+            var result = _client.Process1("1122");
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            var result = _client.Process1("1111");
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            var result = _client.Process1("1234");
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Test04()
+        {
+            var result = _client.Process1("91212129");
+            Assert.AreEqual(9, result);
+        }
+
+        [TestMethod]
+        public void Test05()
+        {
+            var result = _client.Process2("1212");
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Test06()
+        {
+            var result = _client.Process2("1221");
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Test07()
+        {
+            var result = _client.Process2("123425");
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void Test08()
+        {
+            var result = _client.Process2("123123");
+            Assert.AreEqual(12, result);
+        }
+
+        [TestMethod]
+        public void Test09()
+        {
+            var result = _client.Process2("12131415");
+            Assert.AreEqual(4, result);
+        }
+    }
+}
---
+++ advent2017/Days/Day01.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace advent2017.Days
+{
+    public class Day01
+    {
+        public int Process0(string input, Func<int,int, int> nextIdxFunc)
+        {
+            var output = 0;
+
+            for (var i = 0; i < input.Length; ++i)
+            {
+                var current = input[i];
+
+                var nextIdx = nextIdxFunc(i, input.Length);
+
+                var next = input[nextIdx];
+
+                if (current != next) continue;
+
+                var currentInt = int.Parse(current.ToString());
+
+                output += currentInt;
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Reviews a sequence of digits and finds the sum of all digits that match the next digit in the list.
+        /// The list is circular, so the digit after the last digit is the first digit in the list.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public int Process1(string input)
+        {
+            return Process0(input, (i, l) => (i + 1) % l);
+        }
+
+        /// <summary>
+        /// Now, instead of considering the next digit, it wants you to consider the digit halfway around the circular list. 
+        /// That is, if your list contains 10 items, only include a digit in your sum if the digit 10/2 = 5 steps forward matches it. Fortunately, your list has an even number of elements.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public int Process2(string input)
+        {
+            return Process0(input, (i, l) => (i + l / 2) % l);
+        }
+    }
+}
