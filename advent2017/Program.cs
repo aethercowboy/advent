1de091f043a9e9fa390aa29d72c3093cb9c0afd0
Jacob P. Silvia <jacob.silvia@gmail.com>
2017-12-04 10:16:46 -0500

    Adding Runner template to make running suite easier to maintain.

--- advent2017/Program.cs
+++ advent2017/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Linq;
-using advent2017.Data;
-using advent2017.Days;
-using advent2017.Extensions;
 
 namespace advent2017
 {
@@ -14,113 +11,15 @@ namespace advent2017
 
             if (!args.Any())
             {
-                RunAll();
+                Runner.RunAll();
             }
             else
             {
-                switch (args[0])
-                {
-                    case "day01":
-                        Day01(args.NthOrDefault(1));
-                        break;
-                    case "day02":
-                        Day02(args.NthOrDefault(1));
-                        break;
-                    case "day03":
-                        Day03(args.NthOrDefault(1));
-                        break;
-                    case "day04":
-                        Day04(args.NthOrDefault(1));
-                        break;
-                }
-
+                Runner.Run(args);
             }
 
             Console.Write("Press Enter to Quit");
             Console.ReadLine();
-        }
-
-        private static void Day<TDay>(int day, string input)
-            where TDay : IDay, new()
-        {
-            Console.WriteLine($"Day {day:D2}");
-
-            var client = new TDay();
-
-            Console.Write("Part 1: ");
-
-            try
-            {
-                var output1 = client.Part1(input);
-
-                Console.WriteLine(output1);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            Console.Write("Part 2: ");
-
-            try
-            {
-                var output2 = client.Part2(input);
-
-                Console.WriteLine(output2);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-
-        private static void RunAll()
-        {
-            Console.WriteLine("Running All");
-            Day01();
-            Day02();
-            Day03();
-            Day04();
-        }
-
-        private static void Day04(string input = null)
-        {
-            if (input == null)
-            {
-                input = DayData.Day04;
-            }
-
-            Day<Day04>(4, input);
-        }
-
-        private static void Day03(string input = null)
-        {
-            if (input == null)
-            {
-                input = DayData.Day03;
-            }
-
-            Day<Day03>(3, input);
-        }
-
-        private static void Day02(string input = null)
-        {
-            if (input == null)
-            {
-                input = DayData.Day02;
-            }
-
-            Day<Day02>(2, input);
-        }
-
-        private static void Day01(string input = null)
-        {
-            if (input == null)
-            {
-                input = DayData.Day01;
-            }
-
-            Day<Day01>(1, input);
         }
     }
 }
