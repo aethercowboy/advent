6195d86579d8fb374b4f77a4ef21f48f303deaa8
Jacob P. Silvia <jacob.silvia@gmail.com>
2017-12-05 10:02:24 -0500

    Moving Data to a t4 template that references txt files.

--- advent2017/Data/Day03Data.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace advent2017.Data
-{
-    public static partial class DayData
-    {
-        public static string Day03 = "277678";
-    }
-}
---
+++ advent2017/Data/DayData.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+
+namespace advent2017.Data {
+	public static partial class DayData
+	{
+		private static string LoadFile(string day) => File.ReadAllText(Path.Combine("Data", $"{day}.txt"));
+
+		public static string Day01 = LoadFile(nameof(Day01));
+		public static string Day02 = LoadFile(nameof(Day02));
+		public static string Day03 = LoadFile(nameof(Day03));
+		public static string Day04 = LoadFile(nameof(Day04));
+		public static string Day05 = LoadFile(nameof(Day05));
+	}
+}
