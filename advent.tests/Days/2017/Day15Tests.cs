e9be4d6bc759d3a76ef57e52415f6003a202ed13
Jacob P. Silvia <jacob.silvia@gmail.com>
2017-12-17 13:12:12 -0500

    Day 15

---
+++ advent.tests/Days/2017/Day15Tests.cs
@@ -0,0 +1,26 @@
+﻿using advent.Days._2017;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace advent.tests.Days._2017
+{
+    [TestClass]
+    public class Day15Tests : DayTests<Day15>
+    {
+        private const string Input = @"Generator A starts with 65
+Generator B starts with 8921";
+
+        [TestMethod]
+        public void Test01()
+        {
+            var result = Client.Part1(Input);
+            Assert.AreEqual(588, result);
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            var result = Client.Part2(Input);
+            Assert.AreEqual(309, result);
+        }
+    }
+}
--- advent/Days/2017/Day15.cs
+++ advent/Days/2017/Day15.cs
@@ -1,17 +1,110 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using advent.Extensions;
 
 namespace advent.Days._2017
 {
     public class Day15 : Day
     {
+        private static IEnumerable<Generator> GetGenerators(string input)
+        {
+            var factors = new Dictionary<string, int[]>
+            {
+                {"A", new []{16807, 4}},
+                {"B", new []{48271, 8}}
+            };
+
+            var generators = input.Lines()
+                .Select(line => line.Words().ToList())
+                .Select(words => new
+                {
+                    key = words[1],
+                    val = words[4].ToInt(),
+                    fac = factors[words[1]]
+                })
+                .Select(t => new Generator(t.key, t.val, t.fac[0], t.fac[1]))
+                .ToList();
+
+            return generators;
+        }
         public override int Part1(string input)
         {
-            throw new NotImplementedException();
+            var judge = new Judge(40_000_000);
+
+            var generators = GetGenerators(input);
+
+            return judge.Competition(generators, g => g.NextValue());
         }
 
         public override int Part2(string input)
         {
-            throw new NotImplementedException();
+            var judge = new Judge(5_000_000);
+            var generators = GetGenerators(input);
+            return judge.Competition(generators, g => g.NextSpecialValue());
+        }
+    }
+
+    public class Judge
+    {
+        public Judge(int rounds)
+        {
+            Rounds = rounds;
+        }
+
+        public int Rounds { get; set; }
+
+        public int Competition(IEnumerable<Generator> generators, Func<Generator, int> nextFunc)
+        {
+            return Enumerable
+                .Range(0, Rounds)
+                .Select(_ => generators.Select(nextFunc)
+                    .Select(v => Convert.ToString(v, 2).PadLeft(32, '0').Substring(16)).ToList())
+                .Count(values =>
+                {
+                    var first = values.First();
+                    return values.Skip(1).All(x => x == first);
+                });
+        }
+    }
+
+    public class Generator
+    {
+        private const int MagicNumber = 2147483647;
+
+        public Generator(string name, int startingValue, int factor, int denominator)
+        {
+            Name = name;
+            CurrentValue = startingValue;
+            Factor = factor;
+            Denominator = denominator;
+        }
+
+        public string Name { get; set; }
+
+        public int CurrentValue { get; set; }
+        public BigInteger Factor { get; set; }
+        public int Denominator { get; set; }
+
+        public int NextValue()
+        {
+            var current = new BigInteger(CurrentValue);
+
+            var calc = current * Factor;
+            var mod = calc % MagicNumber;
+            CurrentValue = (int)mod;
+            return CurrentValue;
+        }
+
+        public int NextSpecialValue()
+        {
+            do
+            {
+                NextValue();
+            } while (CurrentValue % Denominator != 0);
+
+            return CurrentValue;
         }
     }
 }
