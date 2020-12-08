d0cc7ee3ca1fed8911b29f0a2935ae759c023ab1
Jacob P. Silvia <jacob.silvia@gmail.com>
2020-12-08 09:48:40 -0500

    some code cleanup.

--- advent/Models/2020/Bootcode.cs
+++ advent/Models/2020/Bootcode.cs
@@ -2,55 +2,6 @@
 
 namespace advent.Models._2020
 {
-    public class BootcodeFixer
-    {
-        private readonly string[] _instructions;
-
-        public BootcodeFixer(string[] intructions)
-        {
-            _instructions = intructions;
-        }
-
-        public int Execute()
-        {
-            var bootcode = new Bootcode(_instructions);
-
-            if (bootcode.Execute())
-            {
-                return bootcode.Accumulator;
-            }
-
-            for (var i = 0; i < _instructions.Length; ++i)
-            {
-                var line = _instructions[i];
-
-                if (line.StartsWith("jmp") || line.StartsWith("nop"))
-                {
-                    var newInstructions = new string[_instructions.Length];
-                    _instructions.CopyTo(newInstructions, 0);
-
-                    if (line.StartsWith("jmp"))
-                    {
-                        newInstructions[i] = newInstructions[i].Replace("jmp", "nop");
-                    }
-                    else if (line.StartsWith("nop"))
-                    {
-                        newInstructions[i] = newInstructions[i].Replace("nop", "jmp");
-                    }
-
-                    var newBootcode = new Bootcode(newInstructions);
-
-                    if (newBootcode.Execute())
-                    {
-                        return newBootcode.Accumulator;
-                    }
-                }
-            }
-
-            return 0;
-        }
-    }
-
     public class Bootcode
     {
         public int Accumulator { get; private set; }
@@ -124,7 +75,7 @@ namespace advent.Models._2020
             }
         }
 
-        private int JumpInstruction(char operation, int value)
+        private static int JumpInstruction(char operation, int value)
         {
             if (operation == '+')
             {
